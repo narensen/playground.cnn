5f0abc21a2d36d67399d27477044590368ff1130
Naren Sengodan <scartegy@gmail.com>
2024-07-11 02:40:33 +0530

    adding methods

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
+
+def methods():
+    while True:
+        choice_ = int(input("Next layer to be appended...\n1.Conv2D; 2.Normalization; 3.Dropout; 4.Pooling layer;\n5.Quit\n"))
+
+        if choice_ > 5 or ValueError:
+            print("Please type in the appropriate key\n")
+            pass
+
 
 if __name__ == "__main__":
     print("..playground.cnn")
+    methods()
 
